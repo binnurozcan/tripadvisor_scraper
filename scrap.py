1fe0fd4abdd3d3195bd89a26f02d5a0e6f5792ff
binnurozcan <binnur@outlook.com>
2022-11-21 04:18:44 +0300

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -4,29 +4,34 @@ import pandas as pd
 
 tumyorumlar = []
 
+
 def get_soup(url):
     r = requests.get('http://localhost:8050/render.html', params={'url': url})
     soup = BeautifulSoup(r.text, 'html.parser')
     return soup
-#yorumlar
+
+
+# yorumlar
 def yorumlarial(soup):
-    yorum = soup.find_all('div', {'class','review-container'})
-    #baslik = (item.find('span', {'class': 'noQuotes'}).text.strip())
+    yorum = soup.find_all('div', {'class', 'review-container'})
     try:
         for item in yorum:
             yorum = (item.find('p', {'class': 'partial_entry'}).text.strip())
             tumyorumlar.append(yorum)
     except:
         pass
-#sonraki sayfa
-for x in range(0,15800,15):
-        soup = get_soup(f'https://www.tripadvisor.co.uk/Restaurant_Review-g186338-d4495284-Reviews-or{x}-Aqua_Shard-London_England.html')
-        yorumlarial(soup)
-        print(len(tumyorumlar))
-        if not soup.find('div', {'class': 'nav next ui_button primary'}):
-            pass
-        else:
-            break
+
+
+# sonraki sayfa
+for x in range(0, 15800, 15):
+    soup = get_soup(
+        f'https://www.tripadvisor.co.uk/Restaurant_Review-g186338-d4495284-Reviews-or{x}-Aqua_Shard-London_England.html')
+    yorumlarial(soup)
+    print(len(tumyorumlar))
+    if not soup.find('div', {'class': 'nav next ui_button primary'}):
+        pass
+    else:
+        break
 
 df = pd.DataFrame(tumyorumlar)
 df.to_csv('reviews.csv')
